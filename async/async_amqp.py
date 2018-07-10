685e27e87f90b0f0f3c02df987b22cd767614b27
gaufung <gaufung@outlook.com>
2018-07-10 10:04:56 +0800

    fix model subjects: 1. add AsyncAMQPProducer and AsyncAMQPConsumer comment 2: remove AsyncAMQPProducer queue_name and properties parameters 3: remove AsyncAMQPConsumer queue_name default value

--- async/async_amqp.py
+++ async/async_amqp.py
@@ -5,6 +5,9 @@ from tornado.gen import coroutine
 import pika
 """
 The simple AMQP asynchronous Producer and Consumer 
+
+Usage:
+
 """
 
 __author__ = ["feng.gao@aispeech.com"]
@@ -23,8 +26,18 @@ class AsyncAMQPProducer(AMQObject):
     """
     asynchronous AMQP producer
     """
-    def __init__(self, url, message, exchange_name, routing_key, queue_name=None,
-                 exchange_type="direct", properties=None, io_loop=None):
+    def __init__(self, url, message, exchange_name, routing_key, exchange_type="direct", io_loop=None):
+        """
+        Construct asynchronous AMQP producer
+        :param url: AMQP url, 'amqp://dev:aispeech2018@10.12.7.22:5672/'
+        :param message: message wanted to send.
+        :type message: str or unicode
+        :param exchange_name: exchanger name
+        :type: exchange_name: str
+        :param routing_key: message routing key.
+        :param exchange_type: exchanger type, default is 'direct' for multi-subscribes
+        :param io_loop: io loop. default is tornado.ioloop.IOLoop.current()
+        """
         if io_loop is None:
             io_loop = tornado.ioloop.IOLoop.current()
         self._connection = None
@@ -32,28 +45,27 @@ class AsyncAMQPProducer(AMQObject):
         self._message = message
         self._exchange_name = exchange_name
         self._routing_key = routing_key
-        self._queue_name = queue_name
         self._exchange_type = exchange_type
-        self._properties = properties
         self._io_loop = io_loop
 
     def publish(self):
+        """
+        publish message
+        :return: None
+        """
         self._connection = pika.TornadoConnection(pika.URLParameters(self._url),
                                                   on_open_callback=self._on_open_connection,
                                                   custom_ioloop=self._io_loop)
 
     def _on_open_connection(self, conn):
+        log = self._get_log("_on_open_connection")
+        log.info("open connection")
         conn.channel(on_open_callback=self._on_open_channel)
 
     @coroutine
     def _on_open_channel(self, channel):
         yield tornado.gen.Task(channel.exchange_declare, exchange=self._exchange_name, exchange_type=self._exchange_type)
-        if self._queue_name is not None:
-            yield tornado.gen.Task(channel.queue_declare, queue=self._queue_name)
-            yield tornado.gen.Task(channel.queue_bind, exchange=self._exchange_name,
-                                   queue=self._queue_name)
-        channel.basic_publish(exchange=self._exchange_name, routing_key=self._routing_key, body=self._message,
-                              properties=self._properties)
+        channel.basic_publish(exchange=self._exchange_name, routing_key=self._routing_key, body=self._message)
         channel.close()
 
 
@@ -61,8 +73,20 @@ class AsyncAMQPConsumer(AMQObject):
     """
     asynchronous AMQP producer
     """
-    def __init__(self, url, handler, exchange_name, routing_keys, queue_name=None,
+    def __init__(self, url, handler, exchange_name, routing_keys, queue_name,
                  exchange_type="direct", io_loop=None):
+        """
+        Construct asynchronous AMQP consumer
+        :param url: AMQP url, 'amqp://dev:aispeech2018@10.12.7.22:5672/'
+        :param handler: handle receive message
+        :type: handler: function (get str or unicode parameter and return boolean type. True: success; False: failed.)
+        :param exchange_name: exchanger name
+        :param routing_keys: routing keys
+        :type routing_keys: list or tuple. such as ["error", "info"], it will handle both "error" and "info" categories messages
+        :param queue_name: queue name
+        :param exchange_type: exchanger type: default is 'direct'
+        :param io_loop:  io loop. default is tornado.ioloop.IOLoop.current()
+        """
         if io_loop is None:
             io_loop = tornado.ioloop.IOLoop.current()
         self._connection = None
@@ -75,6 +99,10 @@ class AsyncAMQPConsumer(AMQObject):
         self._io_loop = io_loop
 
     def consume(self):
+        """
+        consume message
+        :return:
+        """
         self._connection = pika.TornadoConnection(pika.URLParameters(self._url),
                                                   on_open_callback=self._on_open_connection,
                                                   custom_ioloop=self._io_loop)
@@ -91,11 +119,7 @@ class AsyncAMQPConsumer(AMQObject):
     def _on_open_channel(self, channel):
         yield tornado.gen.Task(channel.exchange_declare, exchange=self._exchange_name,
                                exchange_type=self._exchange_type)
-        if self._queue_name is None:
-            result = yield tornado.gen.Task(channel.queue_declare, exclusive=True)
-            self._queue_name = result.method.queue
-        else:
-            yield tornado.gen.Task(channel.queue_declare, queue=self._queue_name)
+        yield tornado.gen.Task(channel.queue_declare, queue=self._queue_name)
         for binding_key in self._routing_keys:
             yield tornado.gen.Task(channel.queue_bind, exchange=self._exchange_name,
                                    routing_key=binding_key, queue=self._queue_name)
@@ -110,15 +134,15 @@ if __name__ == "__main__":
     _url = 'amqp://dev:aispeech2018@10.12.7.22:5672/'
     _io_loop = tornado.ioloop.IOLoop.current()
     _exchange_name = "direct_logs"
-    _publish_routing_keys = "debug"
+    _publish_routing_keys = "info"
     _message = "A critical kernel"
     _receive_routing_keys = ["error", "info"]
     _queue_name = "name"
-    c = AsyncAMQPConsumer(_url, callback, _exchange_name, _receive_routing_keys, queue_name="queue", io_loop=_io_loop)
+    p = AsyncAMQPProducer(_url, _message, _exchange_name, _publish_routing_keys, io_loop=_io_loop)
+    p.publish()
+    p = AsyncAMQPProducer(_url, "error kernel", _exchange_name, _publish_routing_keys, io_loop=_io_loop)
+    p.publish()
+    c = AsyncAMQPConsumer(_url, callback, _exchange_name, _receive_routing_keys, "queue_name", io_loop=_io_loop)
     c.consume()
-    p = AsyncAMQPProducer(_url, _message, _exchange_name, _publish_routing_keys, queue_name="queue", io_loop=_io_loop)
-    p.publish()
-    p = AsyncAMQPProducer(_url, "error kernel", _exchange_name, _publish_routing_keys, queue_name="queue", io_loop=_io_loop)
-    p.publish()
     tornado.ioloop.IOLoop().current().start()
 
