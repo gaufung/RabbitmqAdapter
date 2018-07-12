8122c76cb99eb34d968e5bda2b83ba41d22820f3
gaufung <gaufung@outlook.com>
2018-07-12 12:30:32 +0800

    feature subjects: 1. add unitest and coverage 2. add makefile

--- run.py
+++ run.py
@@ -1,8 +1,28 @@
 # -*- encoding:utf-8 -*-
+import os
 import unittest
 import coverage
 
+COV = None
+COV = coverage.coverage(branch=True, include="test/*")
+COV.start()
 
+
+def _test():
+    tests = unittest.TestLoader().discover("test")
+    unittest.TextTestRunner(verbosity=2).run(tests)
+    COV.stop()
+    COV.save()
+    print("Coverage Summary: ")
+    COV.report()
+    basedir = os.path.abspath(os.path.dirname(__file__))
+    covdir = os.path.join(basedir, "tmp/coverage")
+    COV.html_report(directory=covdir)
+    print("HTML version: file://%s//index.html" % (covdir))
+    COV.erase()
+
+
+_test()
 # COV = None
 #
 # import
