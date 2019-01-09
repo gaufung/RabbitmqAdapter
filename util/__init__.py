dc7fecb0d4eb7df63b6b97315fdbda79bd0f16a9
feng.gao <feng.gao@aispeech.com>
2019-01-09 19:06:55 +0800

    add exit

--- util/__init__.py
+++ util/__init__.py
@@ -1,3 +1,4 @@
 # -*- encoding:utf-8 -*-
 from common.util.async_process import AsyncProcess, AsyncProcessPool, AsyncThreadPool
 from common.util.ps import system_monitor
+from common.util.exit import Exit
